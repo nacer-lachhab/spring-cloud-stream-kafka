f2b5ef6cc5b54eb168fdb347e6c0a0b477118050
lachhab nacer <lachhab.nacer@gmail.com>
2022-12-31 01:02:56 +0100

    basic kafka producer

---
+++ src/main/java/com/nacer/springCloudStreamKafka/SpringCloudStreamKafkaApplication.java
@@ -0,0 +1,18 @@
+package com.nacer.springCloudStreamKafka;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamKafkaApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudStreamKafkaApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Bismillah......");
+    }
+}
---
+++ src/main/java/com/nacer/springCloudStreamKafka/web/PageEventRestController.java
@@ -0,0 +1,29 @@
+package com.nacer.springCloudStreamKafka.web;
+
+import java.util.Date;
+import java.util.Random;
+
+import com.nacer.springCloudStreamKafka.entities.PageEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PageEventRestController {
+
+    @Autowired
+    private StreamBridge streamBridge;
+
+    @GetMapping("/publish/{topic}/{name}")//name:nomDePage
+    public PageEvent publish(@PathVariable String topic, @PathVariable String name){
+        PageEvent pageEvent = new PageEvent(
+                                            name,
+                                            Math.random()>0.5?"u1":"u2",
+                                            new Date(),
+                                            new Random().nextInt(500));
+        streamBridge.send(topic,pageEvent);
+        return  pageEvent;
+    }
+}
